c388c94900ae2f0cac342847ce2e8d0fbb103cba
khoadang2929 <khoadangg2002@gmail.com>
2024-12-17 23:38:37 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('.buy-button').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Sản phẩm của bạn đã được thêm vào giỏ hàng!');
+    });
+});
+
+
